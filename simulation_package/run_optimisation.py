ef3ab02f495c9c5462091e0d8ad96f77f1fbe797
gleiria <g.n.leiria@exeter.ac.uk>
2025-06-19 10:14:16 +0100

    changed pymoo object file to optimisation_problem_object.py

--- simulation_package/run_optimisation.py
+++ simulation_package/run_optimisation.py
@@ -1,106 +1,89 @@
-import os
+import time
+import multiprocessing
+import pickle
 import sys
-
-
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import numpy as np
-
-from vivarium.interface import InteractiveContext
-from simulation_package.make_population import Population
-from simulation_package.observer import StateTableObserver
-from simulation_package.autoantibody import AutoAntibody
-from simulation_package.dysglycemia import Dysglycemia
-from simulation_package.from_dysglycemia import FromDysglycemia
-from simulation_package.ab1_to_mab1 import AutoToMultiInsideAutoAntibody
-from simulation_package.mab1_to_ab1 import MultiToAutoInsideAutoantibody
-from simulation_package.type1_diabetes_dka_splitting import Type1DiabetesDkaSplitting
-from simulation_package.ab1_to_healthy import Ab1ToHealthy
-from simulation_package.screening_intervention import ScreeningIntervention
-from simulation_package.screening import Screening
-from simulation_package.objective_function_costs import ObjectiveFunctionCosts
-from simulation_package.objective_function_dka import ObjectiveFunctionDKA
-#
-from simulation_package.uncertain_archiver import Solution, UncertainSol, MeanPerformanceSol, UncertainObjectivesArchiver, UncertainTester
-
+import numpy as np 
 import pymoo
-#from pymoo.core.problem import Problem
-from pymoo.core.problem import ElementwiseProblem
-import numpy as np
+from pymoo.core.problem import Problem
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.optimize import minimize
+from pymoo.core.problem import StarmapParallelization
+from pymoo.operators.mutation.pm import PolynomialMutation
+from simulation_package.optimiser_noisy import NoisyProblem
+from simulation_package.custom_mutation import CustomMutation, CombinedMutation
+#from simulation_package.optimization_call_back import MyCallback
 
 
 
-class NoisyProblem(ElementwiseProblem):
-    total_simulations = 0
 
-    def __init__(self, **kwargs):
+# ----------- timer -------------
+start_time = time.time()
 
-        
-        # calls __init__ method of the super_class Problem so that standard pymoo attributes are initialized
+def generate_diverse_population(n_individuals=100, n_genes=15):
 
-        #super().__init__(n_var=10, n_obj=3, xl = np.zeros(10), xu = np.ones(10), **kwargs)
-        super().__init__(n_var=15, n_obj=3, xl = np.zeros(15), xu = np.ones(15), **kwargs)
+    X = np.random.rand(n_individuals, n_genes)
 
+    # Set the first individual to all 1s
+    X[0, :] = 1
 
-    def _evaluate(self, x, out, *args, **kargs):
-   
+    remaining_individuals = n_individuals - 1
+    screening_spectrum = np.arange(1, n_genes)  
 
-        objective_values_costs = [] 
-        objective_values_dka = []
-        objective_non_zero = []
-        simulation_counter = 0
+    n_per_group = remaining_individuals // len(screening_spectrum)
+    extra = remaining_individuals % len(screening_spectrum)
 
-        seed = None
-        if seed is None:
-            seed = np.random.randint(1,2**32-1)
+    individual_idx = 1
 
-        config = {
-            "randomness": {
-                "key_columns": ["entrance_time", "GRS2", "fdr"],
-                "random_seed": seed,
-            },
-            "population": {"population_size": 100_000},
-            "time": {
-                "step_size": 365,
-            },
-        }
-        # run a simulation for each candidate solution 
-        screening_vector = x
-            
-        
+    for num_zeros in screening_spectrum:
+        n_in_group = n_per_group + (1 if extra > 0 else 0)
+        if extra > 0:
+            extra -= 1
 
-        sim = InteractiveContext(components=[Population(), AutoAntibody(), Ab1ToHealthy(),
-                                    AutoToMultiInsideAutoAntibody(),
-                                    MultiToAutoInsideAutoantibody(),
-                                    Dysglycemia(),
-                                    FromDysglycemia(),
-                                    Screening(continuous_vector= screening_vector),
-                                    ScreeningIntervention('screening_intervention', 'further_t1d_splitting_rate'),
-                                    Type1DiabetesDkaSplitting(dka_ratio=0.58),
-                                    StateTableObserver(),
-                                    ObjectiveFunctionCosts(),
-                                    ObjectiveFunctionDKA()
-                                    ], configuration=config)
+        for _ in range(n_in_group):
+            permuted_indices = np.random.permutation(n_genes)
+            zero_positions = permuted_indices[:num_zeros]
+            X[individual_idx, zero_positions] = 0
+            individual_idx += 1
+
+    return X
 
 
-        sim.take_steps(len(screening_vector))
-        simulation_counter = simulation_counter + 1
-        sim.finalize()
-        # costs
-        obj_function_costs = sim.get_component("objective_function_costs")
-        total_costs = obj_function_costs.total_costs
-        objective_values_costs.append(total_costs)
-        # dka
-        obj_function_dka = sim.get_component("objective_function_dka")
-        dka_ratio = obj_function_dka.dka_ratio
-        objective_values_dka.append(dka_ratio)
-    
-        #non-zero objective (this is independent of simulation)
-        non_zero_counts = sum(1 for value in screening_vector if value > 0)
-        objective_non_zero.append(non_zero_counts)
 
-        out["F"] = np.column_stack([objective_values_costs, objective_values_dka, objective_non_zero])
-        print(out["F"])
-        
+# here it goes
+screening_problem = NoisyProblem()
 
-    
+if __name__ == "__main__":
+    n_processes = 6
+    pool = multiprocessing.Pool(n_processes)
+    runner = StarmapParallelization(pool.starmap)
+
+    screening_problem = NoisyProblem(elementwise_runner=runner)
+      
+    X = generate_diverse_population(n_individuals=10, n_genes=15)
+
+    # mutations
+    default_mutation = PolynomialMutation(prob=0.1, eta=20) # costumize 
+    custom_mutation = CustomMutation(prob=0.3)
+    composite_mutation = CombinedMutation(custom_mutation, default_mutation)
+
+    algo_test = NSGA2(pop_size=10, sampling=X, mutation=composite_mutation)
+
+    num_generations = ("n_gen", 5) #250
+
+
+
+    results = minimize(problem = screening_problem, algorithm = algo_test, termination = num_generations, save_history = True)
+
+
+    with open("local_test.pkl", "wb") as file:
+        res = pickle.dump(results, file)
+
+print("**************************************************************")
+print("--------- executed in %s seconds ---------" % (time.time() - start_time))
+print("**************************************************************")
+
+
+
+
